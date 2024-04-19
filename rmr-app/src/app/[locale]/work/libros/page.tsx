349ba1bb21e15187ec7d39fc1df0f71294bee4f7
saba1998 <saba.mamasakhlisi@lodgify.com>
2024-04-19 20:03:32 +0200

    main page is here build fix

---
+++ rmr-app/src/app/[locale]/work/libros/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rosa Mota Robles | Graphic Designer",
+  description:
+    "MY NAME IS ROSA.I’M A GRAPHIC DESIGNER WITH A MASTERS IN CULTURAL INTELLIGENCE AND INNOVATIONS",
+};
+
+export default function Libros() {
+  return <h1>Libros</h1>;
+}
