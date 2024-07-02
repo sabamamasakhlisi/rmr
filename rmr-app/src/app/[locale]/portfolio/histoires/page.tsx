ed1eb0abf17b32f8269062b343e33e90404be21f
saba1998 <saba.mamasakhlisi@lodgify.com>
2024-07-03 00:19:51 +0200

    responsive fixes

--- rmr-app/src/app/[locale]/portfolio/histoires/page.tsx
+++ rmr-app/src/app/[locale]/portfolio/histoires/page.tsx
@@ -1,4 +1,7 @@
 import { Metadata } from "next";
+import PortfolioContent from "../../components/portfolio_content";
+import Image from "next/image";
+import { useLocale } from "next-intl";
 
 export const metadata: Metadata = {
   title: "rosa mota robles",
@@ -7,5 +10,29 @@ export const metadata: Metadata = {
 };
 
 export default function Histoires() {
-  return <h1>Histoires</h1>;
+  const locale = useLocale();
+  return (
+    <PortfolioContent
+      metadata={metadata}
+      mainContent={
+        <Image
+          src="/5histoires.gif"
+          width={860}
+          priority={true}
+          className="main-image"
+          height={600}
+          quality={100}
+          alt="This short stories fanzine gathers writings from my first 5 months in Paris. Only 1 copy available and only in French."
+        />
+      }
+      description={"< Case study: AESTHETICS OF THE UGLY. >"}
+      sectionContet={
+        <>
+          {locale === "en"
+            ? `This short stories fanzine gathers writings from my first 5 months in Paris. Only 1 copy available and only in French.`
+            : "Este fanzine de relatos cortos reúne escritos de mis primeros 5 meses en París. Tan sólo 1 ejemplar existente y únicamente en francés."}
+        </>
+      }
+    />
+  );
 }
