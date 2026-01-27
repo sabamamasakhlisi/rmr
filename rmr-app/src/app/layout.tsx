0dab9166f5afea965e57f2bddd6a760d219f3667
saba1998 <saba.mamasakhlisi@lodgify.com>
2026-01-27 13:50:59 +0100

    speed insights

--- rmr-app/src/app/layout.tsx
+++ rmr-app/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Header from "./header";
 
 const PPMori = localFont({
@@ -61,6 +63,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
